2ed2c49e2fb56b1ca04feeaed5567a449ba0279f
LEIHhang <48359244+LEIHhang@users.noreply.github.com>
2020-02-21 22:02:17 +0800

    updata

--- p2p_share/main.cpp
+++ p2p_share/main.cpp
@@ -1,8 +1,18 @@
 #include"tool.hpp"
-
-int main()
+#include"httplib.h"
+void HellowWorld(const httplib::Request &res, httplib::Response &rsp)
+{
+	printf("收到一个请求\n");
+	rsp.set_content("<html><h1>hellow world</h1><html>", "text/html");
+	rsp.status = 200;
+}
+int main(int argc,char* argv[])
 {
 	std::vector<Adapter> list;
 	AdapterTool::GetAllAdapter(&list);
+
+	httplib::Server srv;
+	srv.Get("/", HellowWorld);
+	srv.listen("0.0.0.0", 9000);
 	return 0;
 }
